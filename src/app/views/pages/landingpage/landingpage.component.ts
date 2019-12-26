951e13d2263057a5d0974eecf6a93ed49eee6f78
Muhamad Rian Santoso <rian_santoso@ymail.com>
2019-12-26 09:41:46 +0700

    Commit 26 Desember 2019

--- src/app/views/pages/landingpage/landingpage.component.ts
+++ src/app/views/pages/landingpage/landingpage.component.ts
@@ -1,81 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { APIService } from '../../../core/API';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {APIService} from '../../../core/API';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-landingpage',
-  templateUrl: './landingpage.component.html',
-  styleUrls: ['./landingpage.component.scss']
+    selector: 'app-landingpage',
+    templateUrl: './landingpage.component.html',
+    styleUrls: ['./landingpage.component.scss']
 })
 export class LandingpageComponent implements OnInit {
-	
-  model: any;
 
-	listPlaceFrom:any;
-  listPlaceFromWrite:boolean = false;
-  listPlaceTo:any;
-  listPlaceToWrite:boolean = false;
+    model: any;
 
-  landingPageFormGroups: FormGroup;
+    listPlaceFrom: any;
+    listPlaceFromWrite: boolean = false;
+    listPlaceTo: any;
+    listPlaceToWrite: boolean = false;
 
-  constructor(
-  	private http: HttpClient,
-  	private api: APIService,
-    private fb: FormBuilder,
-  ) { }
+    landingPageFormGroups: FormGroup;
 
-  ngOnInit() {
-    this.initLandingPageForm();
-    this.model = {
-        originplace: '',
-        destinationplace: '',
-        outboundpartialdate: '',
-        inboundpartialdate: '',
-      }
-  }
+    constructor(
+        private http: HttpClient,
+        private api: APIService,
+        private fb: FormBuilder,
+    ) {
+    }
 
-  initLandingPageForm(){
-    this.landingPageFormGroups = this.fb.group({
-      originplace: ['', Validators.compose([Validators.required])],
-      destinationplace: ['', Validators.compose([Validators.required])],
-      outboundpartialdate: ['', Validators.compose([Validators.required])],
-      inboundpartialdate: [''],
-    })
-  }
+    ngOnInit() {
+        this.initLandingPageForm();
+        this.model = {
+            originplace: '',
+            originplaceInput: '',
+            destinationplace: '',
+            outboundpartialdate: '',
+            inboundpartialdate: '',
+        };
+    }
 
-  getListPlaceFrom(value: string){
-  	if(value.length >= 2){
-  		this.api.APIListPlace(value).subscribe( (data:any) => {
-  			this.listPlaceFrom = data.Places;
-        this.listPlaceFromWrite = true;
-  		});
-  	} else {
-        this.listPlaceFrom = '';
+    initLandingPageForm() {
+        this.landingPageFormGroups = this.fb.group({
+            originplace: ['', Validators.compose([Validators.required])],
+            originplaceInput: ['', Validators.compose([Validators.required])],
+            destinationplace: ['', Validators.compose([Validators.required])],
+            destinationplaceInput: ['', Validators.compose([Validators.required])],
+            outboundpartialdate: ['', Validators.compose([Validators.required])],
+            inboundpartialdate: [''],
+        });
+    }
+
+    getListPlaceFrom(value: string) {
+        if (value.length >= 2) {
+            this.api.APIListPlace(value).subscribe((data: any) => {
+                this.listPlaceFromWrite = true;
+
+                let placeName = [];
+                data.Places.forEach((data: any) => {
+                    placeName.push(
+                        {placename: data.PlaceName + ', ' + data.PlaceId, placeid: data.PlaceId}
+                    );
+                });
+
+                this.listPlaceFrom = placeName;
+                console.log(this.listPlaceFrom);
+            });
+        } else {
+            this.listPlaceFrom = '';
+            this.listPlaceFromWrite = false;
+        }
+    }
+
+    getListPlaceTo(value: string) {
+        if (value.length >= 2) {
+            this.api.APIListPlace(value).subscribe((data: any) => {
+                this.listPlaceToWrite = true;
+
+                let placeName = [];
+                data.Places.forEach((data: any) => {
+                    placeName.push(
+                        {placename: data.PlaceName + ', ' + data.PlaceId, placeid: data.PlaceId}
+                    );
+                });
+
+                this.listPlaceTo = placeName;
+                console.log(this.listPlaceTo);
+            });
+        } else {
+            this.listPlaceTo = '';
+            this.listPlaceToWrite = false;
+        }
+    }
+
+    clickPlaceFrom(placeid: string, placename: string) {
         this.listPlaceFromWrite = false;
+        this.model.originplace = placeid;
+        this.model.originplaceInput = placename;
     }
-  }
 
-  getListPlaceTo(value: string){
-    if(value.length >= 2){
-      this.api.APIListPlace(value).subscribe( (data:any) => {
-        this.listPlaceTo = data.Places;
-        this.listPlaceToWrite = true;
-      });
-    } else {
-        this.listPlaceTo = '';
+    clickPlaceTo(placeid: string, placename: string) {
         this.listPlaceToWrite = false;
+        this.model.destinationplace = placeid;
+        this.model.destinationplaceInput = placename;
     }
-  }
-
-  clickPlaceFrom(value: string){
-    this.listPlaceFromWrite = false;
-    this.model.originplace = value;
-  }
-
-  clickPlaceTo(value: string){
-    this.listPlaceToWrite = false;
-    this.model.destinationplace = value;
-  }
 
 }
