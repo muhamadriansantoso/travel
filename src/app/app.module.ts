dc56eadb171192f794119f89a8ab7cbc8b61af4c
Muhamad Rian Santoso <rian_santoso@ymail.com>
2019-12-26 10:39:20 +0700

    Commit 26 Desember 2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingpageComponent } from './views/pages/landingpage/landingpage.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LandingpageComponent} from './views/pages/landingpage/landingpage.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {DateAdapter, MAT_DATE_FORMATS, MatNativeDateModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {APP_DATE_FORMATS, AppDateAdapter} from './views/pages/format-datepicker';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingpageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LandingpageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        BrowserAnimationsModule
+    ],
+    providers: [
+        {
+            provide: DateAdapter,
+            useClass: AppDateAdapter
+        },
+        {
+            provide: MAT_DATE_FORMATS,
+            useValue: APP_DATE_FORMATS
+        },
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/views/pages/landingpage/landingpage.component.ts
+++ src/app/views/pages/landingpage/landingpage.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {APIService} from '../../../core/API';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import * as $ from 'jquery';
+import * as dateFormat from 'dateformat';
 
 @Component({
     selector: 'app-landingpage',
@@ -20,22 +21,34 @@ export class LandingpageComponent implements OnInit {
 
     landingPageFormGroups: FormGroup;
 
+    minDate: any;
+
     constructor(
         private http: HttpClient,
         private api: APIService,
         private fb: FormBuilder,
+        private cdr: ChangeDetectorRef
     ) {
     }
 
     ngOnInit() {
         this.initLandingPageForm();
+        const date = new Date();
         this.model = {
             originplace: '',
             originplaceInput: '',
             destinationplace: '',
             outboundpartialdate: '',
             inboundpartialdate: '',
+            minDate: '',
+            defaultDatePlusSevenDay: '',
         };
+
+        this.minDate = dateFormat(new Date(), 'yyyy-mm-dd', 'en');
+        this.model.minDate = this.minDate.toString();
+        this.model.defaultDatePlusSevenDay = dateFormat(new Date(date.getFullYear(), date.getMonth(), date.getDate() + 7), 'yyyy-mm-dd');
+
+        this.cdr.detectChanges();
     }
 
     initLandingPageForm() {
@@ -62,7 +75,6 @@ export class LandingpageComponent implements OnInit {
                 });
 
                 this.listPlaceFrom = placeName;
-                console.log(this.listPlaceFrom);
             });
         } else {
             this.listPlaceFrom = '';
@@ -83,7 +95,6 @@ export class LandingpageComponent implements OnInit {
                 });
 
                 this.listPlaceTo = placeName;
-                console.log(this.listPlaceTo);
             });
         } else {
             this.listPlaceTo = '';
