95a5eb975f77b0afd83c70935742624bc49c7ce2
Muhamad Rian Santoso <rian_santoso@ymail.com>
2020-01-17 16:49:50 +0700

    Commit 17 Januari 2020

--- src/app/views/pages/minutetohour.pipe.ts
+++ src/app/views/pages/minutetohour.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MinuteToHour implements PipeTransform {
     transform(value: number): string {
         const minutes: number = Math.floor(value / 60);
-        return minutes + ' HOUR, ' + (value - minutes * 60) + ' MINUTES';
+        return minutes + 'H ' + (value - minutes * 60) + 'M';
     }
 }
