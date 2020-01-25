b3ede1bd4ad3efb9c769356e858f1b9d59917c20
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-01-25 14:01:26 +0700

    Commit 25 Januari 2020

--- src/app/views/pages/landingpage/flight/flight.component.ts
+++ src/app/views/pages/landingpage/flight/flight.component.ts
@@ -5,97 +5,104 @@ import {FormBuilder} from '@angular/forms';
 import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-flight',
-    templateUrl: './flight.component.html',
-    styleUrls: ['./flight.component.scss']
+  selector: 'app-flight',
+  templateUrl: './flight.component.html',
+  styleUrls: ['./flight.component.scss']
 })
 export class FlightComponent implements OnInit {
 
-    airportList: any;
+  airportList: any;
+  fromCity: string;
+  formCityValue: string;
 
-    constructor(
-        private http: HttpClient,
-        private api: APIService,
-        private fb: FormBuilder,
-        private cdr: ChangeDetectorRef,
-        private _sanitizer: DomSanitizer
-    ) {
-    }
+  constructor(
+    private http: HttpClient,
+    private api: APIService,
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private _sanitizer: DomSanitizer
+  ) {
+  }
 
-    adultPassenger: number;
-    childPassenger: number;
-    infantPassenger: number;
-    roundType: string;
-    public passengersCollapsed: boolean = false;
+  adultPassenger: number;
+  childPassenger: number;
+  infantPassenger: number;
+  roundType: string;
+  public passengersCollapsed: boolean = false;
 
-    ngOnInit() {
-        this.adultPassenger = 1;
-        this.childPassenger = 0;
-        this.infantPassenger = 0;
-        this.roundType = 'oneway';
+  ngOnInit() {
+    this.adultPassenger = 1;
+    this.childPassenger = 0;
+    this.infantPassenger = 0;
+    this.roundType = 'oneway';
 
-        this.api.airportList().subscribe((data: any) => {
-            this.airportList = data;
-            this.cdr.detectChanges();
-        });
-    }
+    this.api.airportList().subscribe((data: any) => {
+      this.airportList = data;
+      this.cdr.detectChanges();
+    });
+  }
 
-    autocompleListFormatter = (data: any) : SafeHtml => {
-        let html = `
+  autocompleListFormatter = (data: any): SafeHtml => {
+    let html = `
         <div style="margin-bottom: 4px;">
             <span style="font-size: 14px; line-height: 20px; font-weight: 500" class="">${data.city}, ${data.country}</span>
         </div>
         <div>
             <span style="color: #8f8f8f; font-size: 12px; line-height: 16px; white-space: normal; font-weight: 400">${data.iata} - ${data.name}</span>
         </div>`;
-        return this._sanitizer.bypassSecurityTrustHtml(html);
-    };
+    return this._sanitizer.bypassSecurityTrustHtml(html);
+  };
 
-    showPassengerIn(){
-        this.passengersCollapsed = !this.passengersCollapsed;
-    }
+  formCityAutoComplete(data) {
+    this.fromCity = data.city + " (" + data.iata + ")";
+    console.log(this.fromCity);
+  }
 
-    showPassengerOut(e: Event){
-        this.passengersCollapsed = false;
-    }
+  showPassengerIn() {
+    this.passengersCollapsed = !this.passengersCollapsed;
+  }
 
-    typeRound(value){
-        this.roundType = value;
-    }
+  showPassengerOut(e: Event) {
+    this.passengersCollapsed = false;
+  }
 
-    adultPassengerMinus() {
-        if (this.adultPassenger > 1){
-            this.adultPassenger = this.adultPassenger - 1;
-        } else {
-            return false;
-        }
-    }
+  typeRound(value) {
+    this.roundType = value;
+  }
 
-    adultPassengerPlus() {
-        this.adultPassenger = this.adultPassenger + 1;
+  adultPassengerMinus() {
+    if (this.adultPassenger > 1) {
+      this.adultPassenger = this.adultPassenger - 1;
+    } else {
+      return false;
     }
+  }
 
-    childPassengerMinus() {
-        if (this.childPassenger > 0){
-            this.childPassenger = this.childPassenger - 1;
-        } else {
-            return false;
-        }
-    }
+  adultPassengerPlus() {
+    this.adultPassenger = this.adultPassenger + 1;
+  }
 
-    childPassengerPlus() {
-        this.childPassenger = this.childPassenger + 1;
+  childPassengerMinus() {
+    if (this.childPassenger > 0) {
+      this.childPassenger = this.childPassenger - 1;
+    } else {
+      return false;
     }
+  }
 
-    infantPassengerMinus() {
-        if (this.infantPassenger > 0){
-            this.infantPassenger = this.infantPassenger - 1;
-        } else {
-            return false;
-        }
-    }
+  childPassengerPlus() {
+    this.childPassenger = this.childPassenger + 1;
+  }
 
-    infantPassengerPlus() {
-        this.infantPassenger = this.infantPassenger + 1;
+  infantPassengerMinus() {
+    if (this.infantPassenger > 0) {
+      this.infantPassenger = this.infantPassenger - 1;
+    } else {
+      return false;
     }
+  }
+
+  infantPassengerPlus() {
+    this.infantPassenger = this.infantPassenger + 1;
+  }
 }
