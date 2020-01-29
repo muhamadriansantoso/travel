9f6c51d51c4074951ae5005078660de9b5a223f2
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-01-29 21:40:33 +0700

    Commit 29 Januari 2020

--- src/app/views/pages/landingpage/landingpage.component.ts
+++ src/app/views/pages/landingpage/landingpage.component.ts
@@ -1,12 +1,6 @@
-import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {APIService} from '../../../core/API';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import * as dateFormat from 'dateformat';
+import {Component, OnInit} from '@angular/core';
 import localeId from '@angular/common/locales/id';
 import {registerLocaleData} from '@angular/common';
-import {finalize, takeUntil, tap} from 'rxjs/operators';
-import {Subject, Subscription} from 'rxjs';
 import {Client} from 'ngx-soap';
 
 
@@ -17,44 +11,19 @@ registerLocaleData(localeId, 'id');
     templateUrl: './landingpage.component.html',
     styleUrls: ['./landingpage.component.scss']
 })
-export class LandingpageComponent implements OnInit, OnDestroy {
+export class LandingpageComponent implements OnInit {
 
     model: any;
     loading: boolean = false;
     searchClicked: boolean = false;
-    selectDepartureExpandCollapse: boolean = false;
-
-    listPlaceFrom: any;
-    listPlaceFromWrite: boolean = false;
-    listPlaceTo: any;
-    listPlaceToWrite: boolean = false;
-
-    browseDatesQuotes: any;
-    browseDatesCarriers: any;
-    browseDatesPlaces: any;
-
-    landingPageFormGroups: FormGroup;
 
     message: string;
     client: Client;
 
-    public submitClicked = false;
-    public flightDetailsCollapsed: boolean[] = [];
-    public priceDetailsCollapsed: boolean[] = [];
-    private unsubscribe: Subject<any>;
-    private subscriptions: Subscription[] = [];
-
-    constructor(
-        private http: HttpClient,
-        private api: APIService,
-        private fb: FormBuilder,
-        private cdr: ChangeDetectorRef,
-    ) {
-        this.unsubscribe = new Subject();
-    }
+  constructor() {
+  }
 
     ngOnInit() {
-
     }
 
     searchClickedEvent(){
@@ -63,262 +32,6 @@ export class LandingpageComponent implements OnInit, OnDestroy {
 
     searchClickedEventOut(){
         this.searchClicked = false;
-    }
-
-    flightDetailsAllCollapsed(value){
-        if (!this.flightDetailsCollapsed[value]){
-            this.flightDetailsCollapsed = [false];
-            this.priceDetailsCollapsed = [false];
-            this.flightDetailsCollapsed[value] = false;
-        } else {
-            this.flightDetailsCollapsed = [false];
-            this.priceDetailsCollapsed = [false];
-            this.flightDetailsCollapsed[value] = true;
-        }
-    }
-
-    priceDetailsAllCollapsed(value){
-        if (!this.priceDetailsCollapsed[value]){
-            this.priceDetailsCollapsed = [false];
-            this.flightDetailsCollapsed = [false];
-            this.priceDetailsCollapsed[value] = false;
-        } else {
-            this.priceDetailsCollapsed = [false];
-            this.flightDetailsCollapsed = [false];
-            this.priceDetailsCollapsed[value] = true;
-        }
-    }
-
-    selectDeparture(){
-        this.selectDepartureExpandCollapse = true;
-    }
-
-    getListPlaceFrom(value: string) {
-        if (value.length >= 2) {
-            this.api.APIListPlace(value).subscribe((data: any) => {
-                this.listPlaceFromWrite = true;
-
-                let placeName = [];
-                data.Places.forEach((data: any) => {
-                    placeName.push(
-                        {placename: data.PlaceName + ', ' + data.PlaceId, placeid: data.PlaceId}
-                    );
-                });
-
-                this.listPlaceFrom = placeName;
-            });
-        } else {
-            this.listPlaceFrom = '';
-            this.listPlaceFromWrite = false;
-        }
-    }
-
-    getListPlaceTo(value: string) {
-        if (value.length >= 2) {
-            this.api.APIListPlace(value).subscribe((data: any) => {
-                this.listPlaceToWrite = true;
-
-                let placeName = [];
-                data.Places.forEach((data: any) => {
-                    placeName.push(
-                        {placename: data.PlaceName + ', ' + data.PlaceId, placeid: data.PlaceId}
-                    );
-                });
-
-                this.listPlaceTo = placeName;
-            });
-        } else {
-            this.listPlaceTo = '';
-            this.listPlaceToWrite = false;
-        }
-    }
-
-    clickPlaceFrom(placeid: string, placename: string) {
-        this.listPlaceFromWrite = false;
-        this.model.originplace = placeid;
-        this.model.originplaceInput = placename;
-    }
-
-    clickPlaceTo(placeid: string, placename: string) {
-        this.listPlaceToWrite = false;
-        this.model.destinationplace = placeid;
-        this.model.destinationplaceInput = placename;
-    }
-
-    flightChooseTabFunc(value){
-        this.model.flightChooseTab = value;
-
-        if (this.model.flightChooseTab == 0) {
-            this.landingPageFormGroups.get('inboundpartialdate').setValidators(Validators.required);
-            this.landingPageFormGroups.get('inboundpartialdate').updateValueAndValidity();
-        } else if (this.model.flightChooseTab == 1) {
-            this.landingPageFormGroups.get('inboundpartialdate').setValidators([]);
-            this.landingPageFormGroups.get('inboundpartialdate').updateValueAndValidity();
-        }
-    }
-
-    onSubmit(){
-        const controls1 = this.landingPageFormGroups.controls;
-
-        if (this.landingPageFormGroups.invalid) {
-            Object.keys(controls1).forEach(controlName =>
-                controls1[controlName].markAsTouched()
-            );
-            return;
-        }
-
-        this.submitClicked = true;
-        this.loading = true;
-
-        var outboundpartialdate = dateFormat(controls1['outboundpartialdate'].value, 'yyyy-mm-dd');
-        var inboundpartialdate = dateFormat(controls1['inboundpartialdate'].value, 'yyyy-mm-dd');
-
-        const authData = {
-            flightChooseTab: controls1['flightChooseTab'].value,
-            originplace: controls1['originplace'].value,
-            originplaceInput: controls1['originplaceInput'].value,
-            destinationplace: controls1['destinationplace'].value,
-            destinationplaceInput: controls1['destinationplaceInput'].value,
-            outboundpartialdate: outboundpartialdate,
-            inboundpartialdate: inboundpartialdate,
-        };
-
-        if(authData.flightChooseTab == 0){
-            this.api.APIBrowseDatesInboound(authData.originplace, authData.destinationplace, authData.outboundpartialdate, authData.inboundpartialdate)
-                .pipe(
-                    tap((data: any) => {
-                        this.browseDatesQuotes = data.Quotes;
-                        this.browseDatesCarriers = data.Carriers;
-                        this.browseDatesPlaces = data.Places;
-                    }),
-                    takeUntil(this.unsubscribe),
-                    finalize(() => {
-                        this.loading = false;
-                        this.cdr.markForCheck();
-                    })
-                )
-                .subscribe(
-                    () => {},
-                    (error:any) => {
-                        if(error.status == '400'){
-
-                        }
-                    }
-                );
-        } else if (authData.flightChooseTab == 1){
-            this.api.APIBrowseDates(authData.originplace, authData.destinationplace, authData.outboundpartialdate)
-                .pipe(
-                    tap((data: any) => {
-                        this.browseDatesQuotes = data.Quotes;
-                        this.browseDatesCarriers = data.Carriers;
-                        this.browseDatesPlaces = data.Places;
-                    }),
-                    takeUntil(this.unsubscribe),
-                    finalize(() => {
-                        this.loading = false;
-                        this.cdr.markForCheck();
-                    })
-                )
-                .subscribe(
-                    () => {},
-                    (error:any) => {
-                        if(error.status == '400'){
-
-                        }
-                    }
-                );
-        }
-
-    }
-
-    showDepartureTimeFlight(){
-        const controls1 = this.landingPageFormGroups.controls;
-
-        if (this.landingPageFormGroups.invalid) {
-            Object.keys(controls1).forEach(controlName =>
-                controls1[controlName].markAsTouched()
-            );
-            return;
-        }
-
-        this.submitClicked = true;
-        this.loading = true;
-
-        var outboundpartialdate = dateFormat(controls1['outboundpartialdate'].value, 'yyyy-mm-dd');
-        var inboundpartialdate = dateFormat(controls1['inboundpartialdate'].value, 'yyyy-mm-dd');
-
-        const authData = {
-            flightChooseTab: controls1['flightChooseTab'].value,
-            originplace: controls1['originplace'].value,
-            originplaceInput: controls1['originplaceInput'].value,
-            destinationplace: controls1['destinationplace'].value,
-            destinationplaceInput: controls1['destinationplaceInput'].value,
-            outboundpartialdate: outboundpartialdate,
-            inboundpartialdate: inboundpartialdate,
-        };
-
-        if(authData.flightChooseTab == 0){
-            this.api.APIBrowseQuotesInboound(authData.originplace, authData.destinationplace, authData.outboundpartialdate, authData.inboundpartialdate)
-                .pipe(
-                    tap((data: any) => {
-                        this.browseDatesQuotes = data.Quotes;
-                        this.browseDatesCarriers = data.Carriers;
-                        this.browseDatesPlaces = data.Places;
-                    }),
-                    takeUntil(this.unsubscribe),
-                    finalize(() => {
-                        this.loading = false;
-                        this.cdr.markForCheck();
-                    })
-                )
-                .subscribe(
-                    () => {},
-                    (error:any) => {
-                        if(error.status == '400'){
-
-                        }
-                    }
-                );
-        } else if (authData.flightChooseTab == 1){
-            this.api.APIBrowseDates(authData.originplace, authData.destinationplace, authData.outboundpartialdate)
-                .pipe(
-                    tap((data: any) => {
-                        this.browseDatesQuotes = data.Quotes;
-                        this.browseDatesCarriers = data.Carriers;
-                        this.browseDatesPlaces = data.Places;
-                    }),
-                    takeUntil(this.unsubscribe),
-                    finalize(() => {
-                        this.loading = false;
-                        this.cdr.markForCheck();
-                    })
-                )
-                .subscribe(
-                    () => {},
-                    (error:any) => {
-                        if(error.status == '400'){
-
-                        }
-                    }
-                );
-        }
-    }
-
-    isControlHasError(controlName: string, validationType: string): boolean {
-        const control = this.landingPageFormGroups.controls[controlName];
-        if (!control) {
-            return false;
-        }
-
-        const result = control.hasError(validationType) && (control.dirty || control.touched);
-        return result;
-    }
-
-    ngOnDestroy(): void {
-        this.unsubscribe.next();
-        this.unsubscribe.complete();
-        this.loading = false;
-        this.subscriptions.forEach(sb => sb.unsubscribe());
     }
 
 }
