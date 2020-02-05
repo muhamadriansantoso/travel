cebeb407b02feb095700bce8c466e37e8b1a279e
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-02-05 15:13:10 +0700

    Commit 5 February 2020

--- src/app/core/API/api.service.ts
+++ src/app/core/API/api.service.ts
@@ -1,91 +1,48 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class APIService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    airportList() {
-        const httpOptions = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        };
-      return this.http.get('http://localhost/apitravel/api/v1/airportlist', httpOptions);
-    }
+  airportList() {
+    const httpOptions = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+    return this.http.get('http://localhost/apitravel/api/v1/airportlist', httpOptions);
+  }
 
   AirLowFareSearchPort(d: string, a: string, date: string, r_date: string, adult: string, child: string, infant: string, cabin: string, type: string) {
-        const httpOptions = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          params: {
-            'departure': d,
-            'arrival': a,
-            'departure_date': date,
-            'return_date': r_date,
-            'adult': adult,
-            'child': child,
-            'infant': infant,
-            'cabin': cabin,
-            'type': type,
-          }
-        };
+    const httpOptions = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      params: {
+        'departure': d,
+        'arrival': a,
+        'departure_date': date,
+        'return_date': r_date,
+        'adult': adult,
+        'child': child,
+        'infant': infant,
+        'cabin': cabin,
+        'type': type,
+      }
+    };
     return this.http.get('http://localhost/apitravel/api/v1/flight/search', httpOptions);
-    }
+  }
 
-    APIListPlace(value: string) {
-        const httpOptions = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'f436a302cfmsh25e5e69bca11548p13862ejsna36226254c59'
-            },
-            params: {'query': value}
-        };
-        return this.http.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/ID/IDR/en-GB/', httpOptions);
-    }
-
-    APIBrowseDates(originplace: string, destinationplace: string, outboundpartialdate:string) {
-        const httpOptions = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'f436a302cfmsh25e5e69bca11548p13862ejsna36226254c59'
-            }
-        };
-        return this.http.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/ID/IDR/en-GB/' +originplace+ '/' +destinationplace+ '/' +outboundpartialdate, httpOptions);
-    }
-
-    APIBrowseDatesInboound(originplace: string, destinationplace: string, outboundpartialdate:string, inboundpartialdate:string) {
-        const httpOptions = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'f436a302cfmsh25e5e69bca11548p13862ejsna36226254c59'
-            }
-        };
-        return this.http.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/ID/IDR/en-GB/' +originplace+ '/' +destinationplace+ '/' +outboundpartialdate+ '/' +inboundpartialdate, httpOptions);
-    }
-
-    APIBrowseRoutes(originplace: string, destinationplace: string, outboundpartialdate:string) {
-        const httpOptions = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'f436a302cfmsh25e5e69bca11548p13862ejsna36226254c59'
-            }
-        };
-        return this.http.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/ID/IDR/en-GB/' +originplace+ '/' +destinationplace+ '/' +outboundpartialdate, httpOptions);
-    }
-
-    APIBrowseQuotesInboound(originplace: string, destinationplace: string, outboundpartialdate:string, inboundpartialdate:string) {
-        const httpOptions = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'f436a302cfmsh25e5e69bca11548p13862ejsna36226254c59'
-            }
-        };
-        return this.http.get('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/ID/IDR/en-GB/' +originplace+ '/' +destinationplace+ '/' +outboundpartialdate+ '/' +inboundpartialdate, httpOptions);
-    }
+  AirPricePort() {
+    var departureTime = encodeURIComponent('2020-12-31T08:25:00.000+07:00');
+    var arrivalTime = encodeURIComponent('2020-12-31T11:50:00.000+08:00');
+    var userData = 'supplier=' + 'travelport1g' + '&origin=' + 'CGK' + '&destination=' + 'KUL' + '&carrier=' + 'GA' + '&departureTime=' + departureTime + '&arrivalTime=' + arrivalTime + '&flightNumber=' + '820';
+    var urlHeader = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
+    return this.http.post('http://localhost/apitravel/api/v1/flight/prebooking', userData, {headers: urlHeader});
+  }
 }
--- src/app/views/pages/prebooking/prebooking.component.ts
+++ src/app/views/pages/prebooking/prebooking.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {APIService} from '../../../core/API';
 
 @Component({
   selector: 'app-prebooking',
@@ -9,11 +10,15 @@ import {Router} from '@angular/router';
 export class PrebookingComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private api: APIService
   ) {
   }
 
   ngOnInit() {
+    this.api.AirPricePort().subscribe((data: any) => {
+      console.log(data);
+    });
   }
 
 }
