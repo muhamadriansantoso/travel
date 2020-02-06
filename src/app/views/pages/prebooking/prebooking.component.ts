cbac694ef15158af7a75e0a0400a9235fc021068
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-02-06 19:08:29 +0700

    Commit 6 February 2020

--- src/app/views/pages/prebooking/prebooking.component.ts
+++ src/app/views/pages/prebooking/prebooking.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {APIService} from '../../../core/API';
+import {FormArray, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-prebooking',
@@ -10,11 +11,14 @@ import {APIService} from '../../../core/API';
 export class PrebookingComponent implements OnInit {
 
   airPricePort: any;
+  airPricePortPassengerNum: any;
+  bookingForm: any;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private api: APIService
+    private api: APIService,
+    private formBuilder: FormBuilder
   ) {
   }
 
@@ -23,7 +27,16 @@ export class PrebookingComponent implements OnInit {
       this.api.AirBookingGetDataDB(sessionID.sessionID).subscribe((AirBookingGetDataDB: any) => {
         this.api.AirPricePort(AirBookingGetDataDB.data).subscribe((AirPricePort: any) => {
           this.airPricePort = AirPricePort.data[0];
-          console.log(this.airPricePort);
+          this.airPricePortPassengerNum = AirPricePort.data[0].passengerType.length;
+
+          this.bookingForm = new FormArray([]);
+
+          for (let i = 0; i < this.airPricePortPassengerNum; i++) {
+            this.bookingForm.push(this.formBuilder.group({
+              name: ['', Validators.required],
+              email: ['', [Validators.required, Validators.email]]
+            }));
+          }
         });
       });
     });
