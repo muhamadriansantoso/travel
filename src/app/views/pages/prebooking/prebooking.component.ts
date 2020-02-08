8010138ea42d1cc17a8b7ffa26bd0ab8ddb23e68
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-02-08 23:57:30 +0700

    Commit 08 Februari 2020

--- src/app/views/pages/prebooking/prebooking.component.ts
+++ src/app/views/pages/prebooking/prebooking.component.ts
@@ -10,6 +10,8 @@ import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class PrebookingComponent implements OnInit {
 
+  sessionID: any;
+  dataFromDB: any;
   airPricePort: any;
   passengerType: any;
   bookingInfoForm: FormGroup;
@@ -26,97 +28,115 @@ export class PrebookingComponent implements OnInit {
   ) {
   }
 
+  get bif() {
+    return this.bookingInfoForm.controls;
+  }
+
+  get df() {
+    return this.bif.dynamicForm as FormArray;
+  }
+
   ngOnInit() {
+    //start get sessionID
+    this.route.params.subscribe(sessionID => {
+      this.sessionID = sessionID.sessionID;
+    });
     //start panggil function initBookingForm
     this.initBookingForm();
     //end panggil function initBookingForm
   }
 
   initBookingForm() {
-    this.route.params.subscribe(sessionID => {
-      this.api.AirBookingGetDataDB(sessionID.sessionID).subscribe((AirBookingGetDataDB: any) => {
-        this.api.AirPricePort(AirBookingGetDataDB.data).subscribe((AirPricePort: any) => {
-          this.airPricePort = AirPricePort.data[0];
-          this.passengerType = AirPricePort.data[0].passengerType;
-          this.passengerLength = AirPricePort.data[0].passengerType.length;
+    //bookingInfoForm
+    this.bookingInfoForm = this.fb.group({
+      title: ['Mr', Validators.compose([
+        Validators.required,
+      ])
+      ],
+      firstname: ['', Validators.compose([
+        Validators.required,
+      ])
+      ],
+      lastname: ['', Validators.compose([
+        Validators.required,
+      ])
+      ],
+      dob: ['', Validators.compose([
+        Validators.required,
+      ])
+      ],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])
+      ],
+      phone: ['', Validators.compose([
+        Validators.required,
+      ])
+      ],
+      dynamicForm: new FormArray([])
+    });
+    //endbookinginfoform
 
-          //bookingInfoForm
-          this.bookingInfoForm = this.fb.group({
-            title: ['Mr', Validators.compose([
-              Validators.required,
-            ])
-            ],
-            firstname: ['', Validators.compose([
-              Validators.required,
-            ])
-            ],
-            lastname: ['', Validators.compose([
-              Validators.required,
-            ])
-            ],
-            dob: ['', Validators.compose([
-              Validators.required,
-            ])
-            ],
-            email: ['', Validators.compose([
-              Validators.required,
-              Validators.email
-            ])
-            ],
-            phone: ['', Validators.compose([
-              Validators.required,
-            ])
-            ],
-          });
-          //endbookinginfoform
+    this.api.AirBookingGetDataDB(this.sessionID).subscribe((AirBookingGetDataDB: any) => {
+      this.dataFromDB = AirBookingGetDataDB;
+      this.api.AirPricePort(this.dataFromDB.data).subscribe((AirPricePort: any) => {
+        this.airPricePort = AirPricePort.data[0];
+        this.passengerType = AirPricePort.data[0].passengerType;
+        this.passengerLength = AirPricePort.data[0].passengerType.length;
 
-          //start looping passenger form dynamically
-          for (var passengerTypeLength = 0; passengerTypeLength < this.passengerLength; passengerTypeLength++) {
-            this.bookingForm[passengerTypeLength] = new FormArray([]);
-            for (var passengerNumLength = 0; passengerNumLength < AirPricePort.data[0].passengerType[passengerTypeLength].numPassenger; passengerNumLength++) {
-              this.bookingForm[passengerTypeLength].push(this.fb.group({
-                passenger_title: ['Mr', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passenger_firstname: ['', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passenger_lastname: ['', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passenger_dob: ['', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passenger_passport: ['', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passenger_passportexpiry: ['', Validators.compose([
-                  Validators.required,
-                ])
-                ],
-                passengerType: AirPricePort.data[0].passengerType[passengerTypeLength].code
-              }));
-            }
+        //start looping passenger form dynamically
+        for (var passengerTypeLength = 0; passengerTypeLength < this.passengerLength; passengerTypeLength++) {
+          for (var passengerNumLength = 0; passengerNumLength < AirPricePort.data[0].passengerType[passengerTypeLength].numPassenger; passengerNumLength++) {
+            this.df.push(this.fb.group({
+              passenger_title: ['Mr', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passenger_firstname: ['', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passenger_lastname: ['', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passenger_dob: ['', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passenger_passport: ['', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passenger_passportexpiry: ['', Validators.compose([
+                Validators.required,
+              ])
+              ],
+              passengerType: AirPricePort.data[0].passengerType[passengerTypeLength].code
+            }));
           }
-          //end looping passenger form dynamically
-        });
+        }
+        //end looping passenger form dynamically
       });
     });
   }
 
   submitBook() {
     this.submitted = true;
-    console.log(this.bookingForm[1]);
     if (this.bookingInfoForm.invalid || this.bookingForm[0].invalid || this.bookingForm[1].invalid || this.bookingForm[2].invalid) {
       const controls = this.bookingInfoForm.controls;
       Object.keys(controls).forEach(controlName =>
         controls[controlName].markAsTouched()
       );
+
+      for (var awal = 0; awal < this.passengerLength; awal++) {
+        for (var awal2 = 0; awal2 < this.airPricePort.passengerType[awal].numPassenger; awal2++) {
+          Object.keys(this.bookingForm[awal].controls[awal2].controls).forEach(controlName =>
+            this.bookingForm[awal].controls[awal2].controls[controlName].markAsTouched()
+          );
+        }
+      }
 
       alert('Data Belum Lengkap');
     } else {
@@ -132,6 +152,19 @@ export class PrebookingComponent implements OnInit {
 
     const result = control.hasError(validationType) && (control.dirty || control.touched);
     return result;
+  }
+
+  isControlHasErrorDynamic(controlName: string, validationType: string): boolean {
+    for (var awal = 0; awal < this.passengerLength; awal++) {
+      for (var awal2 = 0; awal2 < this.airPricePort.passengerType[awal].numPassenger; awal2++) {
+        if (!this.bookingForm[awal].controls[awal2].controls[controlName]) {
+          return false;
+        }
+
+        const result = this.bookingForm[awal].controls[awal2].controls[controlName].hasError(validationType) && (this.bookingForm[awal].controls[awal2].controls[controlName].dirty || this.bookingForm[awal].controls[awal2].controls[controlName].touched);
+        return result;
+      }
+    }
   }
 
 }
