641a153c2a920fcfc104779f64a10b10a2227e29
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-03-10 10:44:22 +0700

    Commit 09 Maret 2020

--- src/app/views/pages/checkorder/checkorder.component.ts
+++ src/app/views/pages/checkorder/checkorder.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {APIService} from '../../../core/API';
 
 @Component({
   selector: 'app-checkorder',
@@ -7,10 +8,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CheckorderComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private api: APIService,
+    private cdr: ChangeDetectorRef
+  ) {
   }
 
   ngOnInit() {
+  }
+
+  submit() {
+    this.api.retrieveBooking('rian_santoso@ymail.com', '1').subscribe((data: any) => {
+      console.log(data);
+      this.cdr.detectChanges();
+    });
   }
 
 }
