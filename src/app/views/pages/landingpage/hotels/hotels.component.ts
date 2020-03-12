3f165816bbd08ddbb8fe5c4f2b95a7405ee41d20
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-03-12 14:53:47 +0700

    Commit 12 Maret 2020

--- src/app/views/pages/landingpage/hotels/hotels.component.ts
+++ src/app/views/pages/landingpage/hotels/hotels.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import * as moment from 'moment';
 import {APIService} from '../../../../core/API';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hotels',
@@ -11,17 +12,26 @@ import {APIService} from '../../../../core/API';
 export class HotelsComponent implements OnInit {
   searchHotelForm: FormGroup;
   defaultCheckInDate: any;
+  defaultCheckInDateFormatted: any;
   minDate: any;
+  geoList: any;
+  geoName: any;
+  geoID: string;
+  durationIndex: any[] = [];
+  durationStayDate: any[] = [];
 
   constructor(
     private api: APIService,
     private fb: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    private _sanitizer: DomSanitizer,
   ) {
   }
 
   ngOnInit() {
     this.api.hotelGeolocation().subscribe((data: any) => {
-      console.log(data)
+      this.geoList = data;
+      this.cdr.detectChanges();
     });
 
     var today = new Date();
@@ -32,6 +42,13 @@ export class HotelsComponent implements OnInit {
       'month': parseInt(moment(todayPlusOne).format('MM'), 0),
       'day': parseInt(moment(todayPlusOne).format('DD'), 0)
     };
+
+    this.defaultCheckInDateFormatted = moment(this.defaultCheckInDate.year + '-' + this.defaultCheckInDate.month + '-' + this.defaultCheckInDate.day).format('YYYY-MM-DD');
+
+    for (var duration = 1; duration <= 14; duration++) {
+      this.durationIndex.push(duration);
+      this.durationStayDate.push(moment(this.defaultCheckInDateFormatted).add(duration, 'days').format('YYYY-MM-DD'));
+    }
 
     this.minDate = {
       'year': today.getFullYear(),
@@ -47,8 +64,33 @@ export class HotelsComponent implements OnInit {
       checkindate: ['', Validators.compose([
         Validators.required,
       ])
+      ],
+      duration: ['1', Validators.compose([
+        Validators.required,
+      ])
       ]
     });
+  }
+
+  autocompleListFormatter = (data: any): SafeHtml => {
+    let html = `
+<div style="margin-bottom: 4px;">
+<span style="font-size: 14px; line-height: 20px; font-weight: 500" class="">${data.name}</span>
+</div>
+<div>
+</div>`;
+    return this._sanitizer.bypassSecurityTrustHtml(html);
+  };
+
+  getGeoID(data) {
+    const dataType = typeof data;
+    if (dataType == 'object') {
+      this.geoName = data.name;
+      this.geoID = data.id;
+    } else {
+      this.geoName = '';
+      this.geoID = '';
+    }
   }
 
   isControlHasError(controlName: string, validationType: string): boolean {
@@ -59,6 +101,16 @@ export class HotelsComponent implements OnInit {
 
     const result = control.hasError(validationType) && (control.dirty || control.touched);
     return result;
+  }
+
+  changeCheckInDate() {
+    this.defaultCheckInDateFormatted = moment(this.defaultCheckInDate.year + '-' + this.defaultCheckInDate.month + '-' + this.defaultCheckInDate.day).format('YYYY-MM-DD');
+    this.durationIndex = [];
+    this.durationStayDate = [];
+    for (var duration = 1; duration <= 14; duration++) {
+      this.durationIndex.push(duration);
+      this.durationStayDate.push(moment(this.defaultCheckInDateFormatted).add(duration, 'days').format('YYYY-MM-DD'));
+    }
   }
 
 }
