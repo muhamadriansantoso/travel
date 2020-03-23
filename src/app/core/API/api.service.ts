48eac2b454d9bac4b8869493781f032bf4f5af81
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-03-23 16:21:32 +0700

    Commit 23 Maret 2020

--- src/app/core/API/api.service.ts
+++ src/app/core/API/api.service.ts
@@ -52,7 +52,7 @@ export class APIService {
         'cabin': cabin,
         'type': type,
         'supplier': supplierData,
-        'before_data': encodeURIComponent(JSON.stringify(beforeData)),
+        'before_data': JSON.stringify(beforeData),
       }
     };
     return this.http.get(APIURL + 'api/v1/flight/search', httpOptions);
