eff6f612ab0b02e6ba6c29cf09770b862e8f24d4
Muhamad Rian Santoso <41132467+muhamadriansantoso@users.noreply.github.com>
2020-06-15 16:28:25 +0700

    API Produk Fix Market

--- src/app/views/pages/search-fixmarket-result/search-fixmarket-result.component.ts
+++ src/app/views/pages/search-fixmarket-result/search-fixmarket-result.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {APIService} from "../../../core/API";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Subject} from "rxjs";
+import {finalize, takeUntil, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-search-fixmarket-result',
@@ -11,6 +12,8 @@ import {Subject} from "rxjs";
 export class SearchFixmarketResultComponent implements OnInit {
   product: any;
   productData: any;
+  searchTitle: any;
+  pleaseWaitLoader: boolean;
 
   private unsubscribe: Subject<any>;
 
@@ -24,13 +27,21 @@ export class SearchFixmarketResultComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.pleaseWaitLoader = true;
     this.route.queryParams.subscribe(params => {
       this.product = params.product;
 
-      this.api.getProductFixMart(this.product).subscribe((data: any) => {
-        this.productData = data.produk;
-        this.cdr.detectChanges();
-      });
+      this.api.getProductFixMart(this.product).pipe(
+        tap((data: any) => {
+          this.searchTitle = data.title;
+          this.productData = data.produk;
+        }),
+        takeUntil(this.unsubscribe),
+        finalize(() => {
+          this.pleaseWaitLoader = false;
+          this.cdr.markForCheck();
+        })
+      ).subscribe();
     });
   }
 
